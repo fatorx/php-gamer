84b7ae4312d107ffc06a8ad4886cee7740275476
Fabio de Souza Mendes <fbiosm@gmail.com>
2021-12-28 10:10:09 -0300

    fix(Config): updated docker-build.sh with in order correct commands

--- docker-build.sh
+++ docker-build.sh
@@ -1,9 +1,11 @@
+cp config/autoload/local.php.dist config/autoload/local.php
+cp .env.dist .env
+
 if [ -f .env ]; then
   export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
 fi
 
 docker-compose down
-cp .env.dist .env
 docker-compose --build
 docker-compose up -d
 docker exec -i ${APP}-php-fpm composer install --ignore-platform-reqs
